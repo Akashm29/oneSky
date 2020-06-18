c806829d8e1c55c76e9fe161c254fae56696c820
akashm29 <akashmishra291997@gmail.com>
2020-06-19 01:05:25 +0530

    added menu transition halfway to get completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
+import Hamburger from "./Hamburger";
+const Header = ({ history }) => {
+  // State for menu button
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu",
+  });
 
-const Header = () => {
-  return <header></header>;
+  // State for disabled button
+  const [disabled, setDisabled] = useState(false);
+
+  // useEffect for page Changes
+  useEffect(() => {
+    // listen for page changes
+    history.listen(() => {
+      setState({ clicked: false, menuName: "Menu" });
+    });
+  });
+
+  const handleMenu = () => {
+    disabledMenu();
+    if (state.initial === false) {
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: "Close",
+      });
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+    }
+  };
+
+  // determine if menu should be disabled
+  const disabledMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 1200);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link to="/">HAMBRG.</Link>
+            </div>
+            <div className="menu">
+              <button onClick={handleMenu} disabled={disabled}>
+                Menu
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Hamburger state={state} />
+    </header>
+  );
 };
 
-export default Header;
+export default withRouter(Header);
